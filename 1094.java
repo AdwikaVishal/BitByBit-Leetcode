1393cf3f65f53fe0e3b26e0d72a484832b0b972a
AdwikaVishal <168633141+AdwikaVishal@users.noreply.github.com>
2025-11-09 19:07:17 +0530

    Create 1094.java

---
+++ 1094.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] stops = new int[1001]; 
+        for (int i = 0; i < trips.length; i++) {
+            int num = trips[i][0];
+            int from = trips[i][1];
+            int to = trips[i][2];
+
+            stops[from] += num;  
+            stops[to] -= num;   
+        }
+
+        int curr = 0;
+        for (int i = 0; i < 1001; i++) {
+            curr += stops[i];
+            if (curr > capacity) {
+                return false; 
+            }
+        }
+
+        return true;
+    }
+}
