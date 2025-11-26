24e098b40768429918f9e61815e3596d959e5a44
AdwikaVishal <168633141+AdwikaVishal@users.noreply.github.com>
2025-11-26 17:58:13 +0530

    Create 151.java

---
+++ 151.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");  
+        List<String> wordList = Arrays.asList(words);
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+}
